c9271a0a1d5f38999836cc78f9bbbe8f768f326b
aq-UI-no <tiffanyaquino116@gmail.com>
2023-09-16 20:57:37 +0300

    pass 1st stage

--- app/server.c
+++ app/server.c
@@ -1,3 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
 int main() {
 	// Disable output buffering
 	setbuf(stdout, NULL);
